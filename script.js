c7a2f87b76a0231e664e2b670da5df8da59ab985
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-02-06 14:59:06 +0600

    Final Commit

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ document
       getPin();
     }
   });
+
+//Handle calculator button event
+const btnContainer = document.getElementById("digit-container");
+btnContainer.addEventListener("click", function (e) {
+  const digit = e.target.innerText;
+  if (isNaN(digit)) {
+    //Handle C
+    if (e.target.innerText == "C") {
+      document.getElementById("typed-pin").value = "";
+    }
+    //Handle backspace
+    if (e.target.innerText == "B") {
+      const str = document.getElementById("typed-pin").value;
+      document.getElementById("typed-pin").value = str.slice(0, str.length - 1);
+    }
+  } else {
+    let typed = document.getElementById("typed-pin");
+    typed.innerText = typed.value += digit;
+  }
+});
+
+//Pin Verify
+function verifyPin() {
+  const matched = document.getElementById("matched");
+  const notMatched = document.getElementById("not-matched");
+  const pin = document.getElementById("display-pin").value;
+  const typed = document.getElementById("typed-pin").value;
+  if (pin == typed) {
+    matched.style.display = "block";
+    notMatched.style.display = "none";
+  } else {
+    matched.style.display = "none";
+    notMatched.style.display = "block";
+  }
+}
